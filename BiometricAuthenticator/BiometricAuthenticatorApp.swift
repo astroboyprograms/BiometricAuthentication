78392a4f1e88964718635d79d0a631bf3e18d2d5
astroboyprograms <jiw7301@nyu.edu>
2022-11-10 00:18:31 -0500

    first commit

---
+++ BiometricAuthenticator/BiometricAuthenticatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricAuthenticatorApp.swift
+//  BiometricAuthenticator
+//
+//  Created by Jordan Ward on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct BiometricAuthenticatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
